4c7f10b3cbf34d906d369a14ea03cfa2b7bba373
Melih Ozan <melihozan867@gmail.com>
2025-08-14 16:31:27 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        bebas: ['Bebas Neue', 'sans-serif'],
+        ptsans: ['PT Sans Narrow', 'sans-serif'],
+        monte: ['MonteCarlo', 'cursive'],
+        atkinson: ['Atkinson Hyperlegible Next', 'sans-serif'],
+      },
+    },
+  },
+};
